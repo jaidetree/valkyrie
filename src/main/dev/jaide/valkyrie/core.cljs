ffd579382cf13308119ec54cab86e6dd148e254b
jaide <jayzawrotny@gmail.com>
2025-03-30 13:23:33 -0400

    Implement some core APIs

--- src/main/dev/jaide/valkyrie/core.cljs
+++ src/main/dev/jaide/valkyrie/core.cljs
@@ -1,41 +1,142 @@
 (ns dev.jaide.valkyrie.core
   (:require
-   [dev.jaide.valhalla/core :as v]))
+   [dev.jaide.valhalla.core :as v]
+   [clojure.pprint :refer [pprint]]))
 
 (defn create
-  [& {:keys [state context] :or {context {}}}]
-  {:current     {:state state
-                 :context context
-                 :effect nil}
+  [& {:keys [validate exhaustive] :as opts
+      :or {validate :true
+           exhaustive false}}]
+  {:state       ::unset
    :transitions {}
    :validators  {:states {}
                  :actions {}}
-   :subscribers []})
+   :opts        opts})
 
 (defn def-state
-  [fsm id validator-map]
-  (-> fsm
-      (assoc-in [:validators :states :d] (v/hash-map validator-map))))
+  [fsm id & [context-validator-map]]
+  (assert (keyword? id) "State id value is required and must be a keyword")
+  (assert (or (nil? context-validator-map)
+              (map? context-validator-map) "Context is an optional hash-map"))
+  (when (fn? (get-in fsm [:validators :states id]))
+    (throw (js/Error. (str "State already defined " (pr-str id)))))
+  (let [context-validator (if (nil? context-validator-map)
+                            (v/literal {})
+                            (v/record context-validator-map))
+        validator (v/record {:value (v/literal id)
+                             :context context-validator})]
+    (-> fsm
+        (assoc-in [:validators :states id] validator))))
+
+(defn- action-validator
+  [id v-map]
+  (v/record (-> {:type (v/literal id)
+                 :meta (v/nilable (v/assert map?))}
+                (into (when (map? v-map)
+                        {:payload (v/record v-map)})))))
 
 (defn def-action
-  [fsm & {:keys [id f validator-map]}]
+  [fsm id & [validator-map]]
+  (assert (keyword? id) "Action id must be a keyword")
+  (assert (or (map? validator-map)
+              (nil? validator-map)) "Validator map must be nil or a hash-map of keys to validators")
+  (when (fn? (get-in fsm [:validators :actions id]))
+    (throw (js/Error. (str "Already defined action for " (pr-str id)))))
   (-> fsm
-      (assoc-in [:validators :actions id] (v/hash-map validator-map))))
+      (assoc-in [:validators :actions id] (action-validator id validator-map))))
 
 (defn- pair-transitions
-  [states actions]
+  [fsm states actions]
+  (doseq [action actions]
+    (when-not (fn? (get-in fsm [:validators :actions action]))
+      (throw (js/Error. (str "Could not find validator for action " action)))))
+  (doseq [state states]
+    (when-not (fn? (get-in fsm [:validators :states state]))
+      (throw (js/Error. (str "Could not find validator for state " state)))))
   (for [state states
         action actions]
     [state action]))
 
 (defn def-transition
-  [fsm {states :in actions :on} f]
-  (let [transitions (pair-transitions states actions)]
+  [fsm {:keys [states actions]} f]
+  (let [transitions (pair-transitions fsm states actions)]
     (loop [fsm fsm
            transitions transitions]
       (let [[transition & transitions] transitions]
+        (when (fn? (get-in fsm [:transitions transition]))
+          (throw (js/Error. (str "Transition already defined for state "
+                                 (pr-str (first transition))
+                                 " and action " (pr-str (second transition))))))
         (if (nil? transition)
           fsm
           (recur
            (assoc-in fsm [:transitions transition] f)
            transitions))))))
+
+(defn init
+  ([fsm state]
+   (init fsm state {}))
+  ([fsm state context]
+   (assert (= (:state fsm) ::unset) "FSM is already initialized")
+   (if-let [validator (get-in fsm [:validators :states state])]
+     (let [fsm (-> fsm
+                   (assoc :state {:value state
+                                  :context context}))
+           result (v/validate validator (:state fsm))]
+       (if (v/valid? result)
+         fsm
+         (throw (js/Error. (str "Invalid state\n" (with-out-str
+                                                    (pprint (:errors result))))))))
+     (throw (js/Error. (str "No validator found for state " (pr-str state)))))))
+
+(defn- assert-action
+  [fsm action]
+  (let [validator (get-in fsm [:validators :actions (:type action)])]
+    (assert (fn? validator) (str "Action not defined, got " (pr-str action)))
+    (-> (v/assert-valid validator action)
+        (:output)
+        (assoc-in [:meta :created-at] (js/Date.now)))))
+
+(defn- assert-transition
+  [fsm action]
+  (let [state (get-in fsm [:state :value])
+        transition (get-in fsm [:transitions [state (:type action)]])]
+    (assert (fn? transition) (str "Transition not defined from state " (pr-str state)
+                                  " from " (pr-str (:type action))))
+    transition))
+
+(defn- assert-state
+  [fsm state]
+  (let [validator (get-in fsm [:validators :states (:value state)])]
+    (assert (fn? validator) (str "State not defined" (pr-str state)))
+    (v/assert-valid validator state)))
+
+(defn- abort
+  [signal-controller]
+  (.abort signal-controller))
+
+(defn- transition-effect
+  [{prev-effect :prev-effect :as prev-state} {next-effect :effect :as next-state}]
+  (when prev-effect
+    (abort (:signal prev-effect)))
+  (if next-effect
+    (let [controller (js/AbortController.)]
+      (-> next-state
+          (assoc-in [:effect :create] next-effect)
+          (assoc-in [:effect :signal] controller)))
+    next-state))
+
+(defn transition
+  [fsm action]
+  (let [action (assert-action fsm action)
+        transition-fn (assert-transition fsm action)
+        prev-state (:state fsm)
+        new-state (-> prev-state
+                      (merge (transition-fn prev-state action)))]
+    [(-> fsm
+         (assoc :state new-state))
+     {:prev prev-state
+      :next new-state
+      :at (js/Date.now)}]))
+
+
--- src/test/dev/jaide/valkyrie/core_test.cljs
+++ src/test/dev/jaide/valkyrie/core_test.cljs
@@ -1,9 +1,149 @@
 (ns dev.jaide.valkyrie.core-test
   (:require
-   [clojure.test :refer [deftest testing is]]))
+   [clojure.pprint :refer [pprint]]
+   [clojure.test :refer [deftest testing is]]
+   [dev.jaide.valkyrie.core :as fsm]
+   [dev.jaide.valhalla.core :as v]))
 
-(deftest fsm-test
-  (testing "fsm-test"
-    (is (= 1 1))))
+(deftest create-test
+  (testing "create"
+    (testing "Returns a fsm hash-map"
+      (let [fsm (fsm/create)]
+        (is (map? fsm) "FSM was not a hash-map")
+        (is (= (:state fsm) :dev.jaide.valkyrie.core/unset))))))
+
+(deftest def-state-test
+  (testing "def-state"
+    (testing "without context"
+      (let [fsm (fsm/create)
+            fsm (fsm/def-state fsm :idle)
+            validator (get-in fsm [:validators :states :idle])]
+        (is (fn? validator))
+        (is (v/valid? (v/validate validator {:value :idle
+                                             :context {}})))))
+    (testing "with context"
+      (let [fsm (fsm/create)
+            fsm (fsm/def-state fsm :pending {:url (v/string)})
+            validator (get-in fsm [:validators :states :pending])]
+        (is (fn? validator))
+        (is (v/valid? (v/validate validator {:value :pending
+                                             :context {:url "https://example.com"}})))))
+    (testing "throws error if state already defined"
+      (let [fsm (fsm/create)
+            fsm (fsm/def-state fsm
+                  :pending
+                  {:url (v/string)})]
+        (is (thrown? :default (fsm/def-state fsm :pending {:url (v/number)})))))))
+
+(deftest def-action-test
+  (testing "def-action"
+    (testing "define action"
+      (let [fsm (fsm/create)
+            fsm (fsm/def-action fsm
+                  :run-test
+                  {:test-id (v/string)})
+            validator (get-in fsm [:validators :actions :run-test])]
+        (is (fn? validator))
+        (is (v/valid? (v/validate validator {:type :run-test
+                                             :payload {:test-id "this-test"}})))))
+    (testing "throws error if already defined"
+      (let [fsm (fsm/create)
+            fsm (fsm/def-action fsm
+                  :run-test
+                  {:test-id (v/string)})]
+        (is (thrown? :default (fsm/def-action fsm :run-test {:test-id (v/number)})))))))
+
+(def test-fsm
+  (-> (fsm/create)
+      (fsm/def-state :green-light)
+      (fsm/def-state :yellow-light)
+      (fsm/def-state :red-light)
+      (fsm/def-action :change-light)))
+
+(deftest def-transition-test
+  (testing "def-transition"
+    (testing "define map of transitions"
+      (let [fsm test-fsm
+            fsm (fsm/def-transition
+                  fsm
+                  {:states [:green-light :yellow-light :red-light]
+                   :actions [:change-light]}
+                  (fn [{:keys [state _context _effect]} action]
+                    (case [state action]
+                      [:green-light :change-light] {:state :yellow-light}
+                      [:yellow-light :change-light] {:state :red-light}
+                      [:red-light :change-light] {:state :green-light})))]
+        (is (fn? (get-in fsm [:transitions [:green-light :change-light]])))
+        (is (fn? (get-in fsm [:transitions [:yellow-light :change-light]])))
+        (is (fn? (get-in fsm [:transitions [:red-light :change-light]])))))
+
+    (testing "throws error on overlapping defs"
+      (let [fsm test-fsm
+            fsm (fsm/def-transition
+                  fsm
+                  {:states [:green-light :yellow-light :red-light]
+                   :actions [:change-light]}
+                  (fn [{:keys [state _context _effect]} action]
+                    (case [state action]
+                      [:green-light :change-light] {:state :yellow-light}
+                      [:yellow-light :change-light] {:state :red-light}
+                      [:red-light :change-light] {:state :green-light})))]
+        (is
+         (thrown? :default
+                  (fsm/def-transition
+                    fsm
+                    {:states [:yellow-light]
+                     :actions [:change-light]}
+                    (fn [{:keys [state _context _effect]} action]
+                      {:state :red-light}))))))
+
+    (testing "throws error if state was not defined"
+      (let [fsm test-fsm
+            fsm (fsm/def-transition
+                  fsm
+                  {:states [:green-light :yellow-light :red-light]
+                   :actions [:change-light]}
+                  (fn [{:keys [state _context _effect]} action]
+                    (case [state action]
+                      [:green-light :change-light] {:state :yellow-light}
+                      [:yellow-light :change-light] {:state :red-light}
+                      [:red-light :change-light] {:state :green-light})))]
+        (is
+         (thrown? :default
+                  (fsm/def-transition
+                    fsm
+                    {:states [:purple-light]
+                     :actions [:change-light]}
+                    (fn [{:keys [state _context _effect]} action]
+                      {:state :red-light}))))))
+
+    (testing "throws error if action was not defined"
+      (let [fsm test-fsm
+            fsm (fsm/def-transition
+                  fsm
+                  {:states [:green-light :yellow-light :red-light]
+                   :actions [:change-light]}
+                  (fn [{:keys [state _context _effect]} action]
+                    (case [state action]
+                      [:green-light :change-light] {:state :yellow-light}
+                      [:yellow-light :change-light] {:state :red-light}
+                      [:red-light :change-light] {:state :green-light})))]
+        (is
+         (thrown? :default
+                  (fsm/def-transition
+                    fsm
+                    {:states [:green-light :yellow-light :red-light]
+                     :actions [:flash-light]}
+                    (fn [{:keys [state _context _effect]} action]
+                      {:state :red-light}))))))))
+
+(deftest init-test
+  (testing "init"
+    (testing "Sets initial state on fsm"
+      (let [fsm (fsm/init test-fsm :green-light)]
+        (is (= (get-in fsm [:state :value])
+               :green-light))
+        (is (= (get-in fsm [:state :context])
+               {}))))))
 
 
